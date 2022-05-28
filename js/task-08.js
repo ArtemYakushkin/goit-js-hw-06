481cc29a8cf0a8024f8afa2e0916f0fa9621f695
ArtemYakushkin <zimberger29101985@gmail.com>
2022-05-28 22:20:41 +0300

    add task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const form = document.querySelector('.login-form');
+// const emailEl = document.querySelector('input[type="email"]').value;
+// const passwordEl = document.querySelector('input[type="password"]').value;
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTurget);
+    console.log(formData);
+
+    const {
+        elements: { email, password }
+    } = event.currentTarget;
+
+    if (email.value === "" || password.value === "") {
+        alert('All fields must be filled!');
+    }
+    
+    document.querySelector('.login-form').reset();
+}
